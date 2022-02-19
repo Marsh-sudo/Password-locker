51a2c2ffdcbb13923419e3ca6297b890b3d57f6c
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-02-19 15:33:26 +0300

    create  function to delete user

--- run.py
+++ run.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3.6
 from passlocker import User,Credentials
 
-def create_contact(account,username,password):
+def create_users(account,username,password):
     '''
     Function to create a new account details
     '''
 
-    new_account = User(account,username,password)
-    return new_account
+    new_user = User(account,username,password)
+    return new_user
+
+def save_users(user):
+    '''
+    function to save user
+    '''
+    user.save_user()
+
+def del_user(user):
+    '''
+    function to delete a user
+    '''
+    user.delete_user()
