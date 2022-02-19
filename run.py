ff1761aec351e2c1ceafe4983e24fb69d11964ed
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-02-19 15:48:51 +0300

    function to show existing user

--- run.py
+++ run.py
@@ -20,3 +20,17 @@ def del_user(user):
     function to delete a user
     '''
     user.delete_user()
+
+  #finding a user
+def find_user(username):
+    '''
+    function that finds a user by account and returns the contact
+    '''
+    return Credentials.find_by_username(username)
+
+#user existence
+def check_existing_users(username):
+    '''
+    function that check if a user exist with that username and returns a boolean
+    '''
+    return Credentials.user_exist(username)
