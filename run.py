ac614b41e751c071047aa501858148aa9488d9c4
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-02-19 15:25:13 +0300

    create function to implement creating of new account

--- run.py
+++ run.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3.6
 from passlocker import User,Credentials
+
+def create_contact(account,username,password):
+    '''
+    Function to create a new account details
+    '''
+
+    new_account = User(account,username,password)
+    return new_account
